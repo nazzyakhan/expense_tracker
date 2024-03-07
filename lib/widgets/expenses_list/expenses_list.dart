658a187ec1cc401ba8b68a8faa5deb32172a9215
nazzyakhan <najiyakhan0p@gmail.com>
2024-03-07 14:21:00 +0530

    changed

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
-class ExpensesList extends StatelessWidget {
+class ExpensesList extends StatefulWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
@@ -12,17 +12,30 @@ class ExpensesList extends StatelessWidget {
   final void Function(Expense expense) onRemoveExpense;
 
   final List<Expense> expenses;
+
+  @override
+  State<ExpensesList> createState() => _ExpensesListState();
+}
+
+class _ExpensesListState extends State<ExpensesList> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (context, index) => Dismissible(
-        onDismissed: (direction) {
-          expenses.remove(expenses[index]);
-        },
-        key: ValueKey(expenses[index]),
-        child: ExpenseItem(expense: expenses[index]),
-      ),
-    );
+    return widget.expenses.isEmpty
+        ? const Center(child: Text("No expenses found. Start adding some!"))
+        : ListView.builder(
+            itemCount: widget.expenses.length,
+            itemBuilder: (context, index) {
+              print("building $index");
+              return Dismissible(
+                onDismissed: (direction) {
+                  print("dismissing $index");
+                  setState(
+                      () => widget.expenses.remove(widget.expenses[index]));
+                },
+                key: ValueKey(widget.expenses[index]),
+                child: ExpenseItem(expense: widget.expenses[index]),
+              );
+            },
+          );
   }
 }
